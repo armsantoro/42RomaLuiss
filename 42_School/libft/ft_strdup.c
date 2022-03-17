d05473ac9bd618b69bde94b265b8d0d65d71c19a
Armando <67610907+armsantoro@users.noreply.github.com>
2022-03-17 11:42:43 +0100

    add calloc & strdup

--- 42_School/libft/ft_strdup.c
+++ 42_School/libft/ft_strdup.c
@@ -1,26 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: spyro <spyro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/16 14:56:32 by asantoro          #+#    #+#             */
-/*   Updated: 2022/03/16 23:44:26 by spyro            ###   ########.fr       */
+/*   Created: 2022/03/16 23:34:46 by spyro             #+#    #+#             */
+/*   Updated: 2022/03/17 11:16:13 by spyro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+char	*ft_strdup(const char *s)
 {
-	size_t	i;
-	void	*str;
+	int		size;
+	char	*str;
 
-	str = malloc(size * count);
-	if (!str)
-		return (NULL);
-		i = 0;
-	ft_calloc(str, size * count)
+	size = ft_strlen((char *)s);
+	str = malloc(size);
+	str = (char *)s;
 	return (str);
 }
--- 42_School/libft/libft.h
+++ 42_School/libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asantoro <asantoro@student.42.fr>          +#+  +:+       +#+        */
+/*   By: spyro <spyro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 14:25:45 by asantoro          #+#    #+#             */
-/*   Updated: 2022/03/16 11:25:29 by asantoro         ###   ########.fr       */
+/*   Updated: 2022/03/17 11:28:26 by spyro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s);
 
 #endif
