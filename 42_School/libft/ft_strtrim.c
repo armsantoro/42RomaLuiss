cc31b061a2ffef6e1eac8599422e1a924dff49f9
Armando <67610907+armsantoro@users.noreply.github.com>
2022-03-18 12:11:05 +0100

    STRTRIM

--- 42_School/libft/ft_strtrim.c
+++ 42_School/libft/ft_strtrim.c
@@ -6,48 +6,61 @@
 /*   By: spyro <spyro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 17:56:48 by spyro             #+#    #+#             */
-/*   Updated: 2022/03/17 22:13:09 by spyro            ###   ########.fr       */
+/*   Updated: 2022/03/18 11:43:22 by spyro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-bool	is_match(char c, char const *str)
+int	is_match(char set, char const *str)
 {
 	while (*str)
 	{
-		if (*str == c)
-			return (true);
+		if (*str == set)
+			return (1);
 		str++;
 	}
-	return (false);
+	return (0);
 }
 
-char	*ft_strtrim(const char *s1, const char *set)
+char	trim_char(char set, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == set)
+			str[i] = 127;
+		i++;
+	}
+	return ((char)*str);
+}
+
+char	*ft_strtrim(const char *s1, char *set)
 {
 	int		i;
-	int		j;
-	int		k;
+	int		size_s_trim;
 	char	*strim;
 
 	if (!s1 || !set)
 		return (NULL);
 	i = 0;
-	j = 0;
-	while (s1[j] && is_match(s1[j], set))
-		j++;
-	k = ft_strlen((char *) s1);
-	while (k > j && is_match(s1[k -1], set))
-		k++;
-	strim = (char *) malloc(sizeof(*s1) * (k - j + 1));
-	
-
+	while (s1[i] && is_match(set, s1[i]))
+	{
+		trim_char((char) set, (char *)s1[i]);
+		i++;
+	}
+	size_s_trim = ft_strlen((char *)s1);
+	strim = malloc(size_s_trim + 1);
+	*strim = *s1;
+	return (strim);
 }
 
-#include <stdio.h>
 int	main()
 {
 	char a[] = "mamma";
 	char b[] = "a";
-	printf("%s\n", strim(a, b));
+	printf("%s\n", ft_strtrim(a, b));
 }
